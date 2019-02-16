6468099cc0fe0b780f9c3335db1ad9c4b4bbbcdb
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-02-16 01:19:22 -0700

    made cut work with returns

---
+++ data/NC/README.md
@@ -0,0 +1,18 @@
+This dataset was generated from a full copy of the data file
+`2017.annual.singlefile.csv` using a version of `tt.py` containing the
+'startgrep' subcommand and this sequence of commands:
+
+## Linux
+
+    $ python tt.py head -n 1 2017.annual.singlefile.csv > header.csv
+    $ python tt.py startgrep '"37' 2017.annual.singlefile.csv > dat.csv
+    $ python tt.py cat header.csv dat.csv > 2017.annual.singlefile.csv
+    $ rm header.csv dat.csv
+
+
+## Windows
+
+    C:\> python tt.py head -n 1 2017.annual.singlefile.csv > header.csv
+    C:\> python tt.py startgrep """37" 2017.annual.singlefile.csv > dat.csv
+    C:\> python tt.py cat header.csv dat.csv > 2017.annual.singlefile.csv
+    C:\> del header.csv dat.csv
--- src/CutPaste.py
+++ src/CutPaste.py
@@ -1,62 +1,67 @@
 def cut(args):
-    """remove sections from each line of files"""
-
+    """remove sections from each column of csv files"""
     if args[0] == "-f":
 
         # takes in one or multiple column args.
-        iden = str(args[1])
+        iden = args[1:-1]
+        # one or multiple identifiers in a list can come out
 
-        iden = iden.split(",")
         try:
             for i in range(len(iden)):
                 iden[i] = int(iden[i])
+
         except ValueError:
             print("Indexes must be positive integers.")
 
         # print multiple specific columns
         if len(iden) > 1:
-            print_cut(args[2], iden)
+            print("mc")
+            return print_cut(args[-1], iden)
 
         # print one specific column
         else:
-            print_cut(args[2], iden[0])
+            print("oc")
+            return print_cut(args[-1], iden[0])
 
     else:
-        print_cut(args[0], 0)
+        return(print_cut(args, 0))
 
 
 def print_cut(file, column):
-    try:
-        with open(file) as data:
-            lines = data.readlines()
+    lst = []
+    if not isinstance(column, list):  # single column
+        try:
+            for line in open(file):
+                line_list = line.strip().split(',')
 
-        # remove \n left over from .readlines()
-        for i in range(len(lines)):
-            lines[i] = lines[i][:-1]
+                if not isinstance(column, list):
+                    try:
+                        lst.append(line_list[column])
+                    except IndexError:
+                        print("The index you attempted doesn't exist in this file.", "cut")
 
-        # splits lines by commas into lists
-        for i in range(len(lines)):
-            lines[i] = lines[i].split(",")
+        except FileNotFoundError:
+            print("File not found, go to cut.py")
 
-        # prints one column
-        if not isinstance(column, list):
-            try:
-                for line in lines:
-                    print(line[column])
-            except IndexError:
-                print("The index you attempted doesn't exist in this file.")
-
-        # prints multiple columns
-        else:
-            for line in lines:
+    else:  # multiple columns
+        try:
+            lst.append('')
+            j = 0
+            for line in open(file):
+                line_list = line.strip().split(',')
                 for i in range(len(column)):
-                    print(line[column[i]], end="")
+                    lst[j] = lst[j] + line_list[column[i]]
                     if i < len(column) - 1:
-                        print(", ", end="")
-                print("")
+                        lst[j] = lst[j] + ' ,'
+                lst.append('')
+                j += 1
 
-    except FileNotFoundError:
-        print("Given file not found.")
+        except FileNotFoundError:
+            print("File not Found, go to cut.py")
+
+    while '' in lst:
+        lst.remove('')
+    return lst
 
 
 def paste(args):
@@ -68,14 +73,13 @@ def paste(args):
             with open(file) as stuff:
                 lines = stuff.readlines()
 
-            # remove \n left over from .readlines()
             for i in range(len(lines)):
-                lines[i] = lines[i][:-1]
+                lines[i] = lines[i].strip('\n')
 
             data.append(lines)
 
         except FileNotFoundError:
-            print("Given file not found.")
+            print("File not found, go to paste.py")
 
     # find the longest file
     max = 0
