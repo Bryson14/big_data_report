b0c872cf7f1a2ecd2d0e3b8ad0bf94ab1328effd
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-02-16 01:20:38 -0700

    skeleton started for project

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
 import sys
-from Partial import head
+from Grep import grep, endgrep, startgrep
+from CutPaste import cut
+from Report import Report
 
-if __name__ == "__main__":
 
+def dict_area_titles(d) -> dict:
+    print("d: ", d)
+    fil = d + '\\area_titles.csv'
+
+    codes = {}
+
+    myfil = d + r'\2017.annual.singlefile.csv'
+    myfil = r'C:\Users\Bryson M\Desktop\Methods in CS\Assn2-Text_Tools\data\people.csv'
+    results = cut(['-f',0,1,2,myfil])
+    print(results)
+
+
+def find_report_data(d: str):
+    pass
+
+
+def main():
     # too little arguments
     if len(sys.argv) < 2:
         print("Usage: src\main.py DATA_DIRECTORY")
@@ -10,59 +28,58 @@ if __name__ == "__main__":
 
     # initializes the program
     elif len(sys.argv) == 2:
-        try:
-            head(sys.argv[1:])
-
-        except FileNotFoundError:
-            print("File not found.")
-        sys.exit(1)
+        dict_area_titles(sys.argv[1])
 
     # unidentified function call
     else:
         print("Too many arguments given\nUsage: src\main.py DATA_DIRECTORY")
         sys.exit(1)
 
-# Convert `area_titles.csv` into a dictionary
-#############################################
 
-
-# Collect stats from `2017.annual.singlefile.csv`
-#################################################
-
-
-# Create the report object
-##########################
-rpt = Report()
-
-
-# Fill in the report for all industries
-#######################################
-rpt.all.num_areas           = 1337
-
-rpt.all.gross_annual_wages  = 13333337
-rpt.all.max_annual_wage     = ("Trantor", 123456)
-
-rpt.all.total_estab         = 42
-rpt.all.max_estab           = ("Terminus", 12)
-
-rpt.all.total_empl          = 987654
-rpt.all.max_empl            = ("Anacreon", 654)
-
-
-# Fill in the report for the software publishing industry
-#########################################################
-rpt.soft.num_areas          = 1010
-
-rpt.soft.gross_annual_wages = 101001110111
-rpt.soft.max_annual_wage    = ("Helicon", 110010001)
-
-rpt.soft.total_estab        = 1110111
-rpt.soft.max_estab          = ("Solaria", 11000)
-
-rpt.soft.total_empl         = 100010011
-rpt.soft.max_empl           = ("Gaia", 10110010)
-
-
-# Print the completed report
-############################
-print(rpt)
+if __name__ == "__main__":
+    main()
+#
+# # Convert `area_titles.csv` into a dictionary
+# #############################################
+#
+#
+# # Collect stats from `2017.annual.singlefile.csv`
+# #################################################
+#
+#
+# # Create the report object
+# ##########################
+# rpt = Report()
+#
+#
+# # Fill in the report for all industries
+# #######################################
+# rpt.all.num_areas           = 1337
+#
+# rpt.all.gross_annual_wages  = 13333337
+# rpt.all.max_annual_wage     = ("Trantor", 123456)
+#
+# rpt.all.total_estab         = 42
+# rpt.all.max_estab           = ("Terminus", 12)
+#
+# rpt.all.total_empl          = 987654
+# rpt.all.max_empl            = ("Anacreon", 654)
+#
+#
+# # Fill in the report for the software publishing industry
+# #########################################################
+# rpt.soft.num_areas          = 1010
+#
+# rpt.soft.gross_annual_wages = 101001110111
+# rpt.soft.max_annual_wage    = ("Helicon", 110010001)
+#
+# rpt.soft.total_estab        = 1110111
+# rpt.soft.max_estab          = ("Solaria", 11000)
+#
+# rpt.soft.total_empl         = 100010011
+# rpt.soft.max_empl           = ("Gaia", 10110010)
+#
+#
+# # Print the completed report
+# ############################
+# print(rpt)
